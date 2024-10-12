855c6ed112be3842615e245fd2e25ed4ba80032a
hashim-salah-alden <82958890+hashim-salah-alden@users.noreply.github.com>
2024-10-12 17:43:14 +0300

    Update axios-global.js

--- frontend/src/services/axios-global.js
+++ frontend/src/services/axios-global.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://chat-app-mern-stack-2v3d.onrender.com",
   withCredentials: true
 });
 
